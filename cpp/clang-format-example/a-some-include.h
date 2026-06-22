b510eab0859955a251b80e0133497d18d6df37a0
rebecca <rebeccarashkin@gmail.com>
2026-06-21 19:15:11 -0700

    style: Updated clang-format include order

---
+++ cpp/clang-format-example/a-some-include.h
@@ -0,0 +1,16 @@
+
+// clang-format off
+#include "rabbit.h"
+#include "bunny.h"
+// clang-format on
+
+#include "orange.h"
+
+#include "apple.h"
+
+#include "a.h"
+#include "b.h"
+#include "c.h"
+
+#include "bar.h"
+#include "foo.h"
